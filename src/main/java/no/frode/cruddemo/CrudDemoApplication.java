a342c7aad954c755d0e7b25b6730c62f586927b5
Frodo <frodo@bagend.com>
2019-05-25 14:37:31 +0200

    Added default profile

--- src/main/java/no/frode/cruddemo/CrudDemoApplication.java
+++ src/main/java/no/frode/cruddemo/CrudDemoApplication.java
@@ -1,22 +1,64 @@
 package no.frode.cruddemo;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 @EnableCaching
 public class CrudDemoApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(CrudDemoApplication.class);
+
+    @Autowired
+    Environment env;
+
     public static void main(String[] args) {
-        SpringApplication.run(CrudDemoApplication.class, args);
+        logger.info("__Starting CrudDemoApplication");
+
+        SpringApplication app = new SpringApplication(CrudDemoApplication.class);
+
+        String environement = System.getenv("ENVIRONMENT");
+
+        if (environement == null) {
+            environement = "dev";
+            app.setAdditionalProfiles(environement);
+        }
+
+
+        if (environement != null) {
+            app.setAdditionalProfiles(environement.toLowerCase());
+        }
+
+        app.run(args);
+        //SpringApplication.run(CrudDemoApplication.class, args);
     }
 
+    void checkProfiles() {
+        String[] profiles = env.getActiveProfiles();
+
+        for (String s : profiles) {
+            logger.info("__profile {}",s);
+        }
+
+    }
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public void checkEnironments() {
+        String[] profiles = env.getDefaultProfiles();
+        logger.info(profiles.toString());
     }
 
 }
