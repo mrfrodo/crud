766f9a3827b914d00a88705d56174f072297e838
Frodo <frodo@bagend.com>
2019-06-12 12:15:52 +0200

    Configure a database programatically

--- src/main/java/no/frode/cruddemo/configuration/CrudDemoApplicationConfig.java
+++ src/main/java/no/frode/cruddemo/configuration/CrudDemoApplicationConfig.java
@@ -1,11 +1,5 @@
 package no.frode.cruddemo.configuration;
 
-import org.ehcache.config.builders.CacheConfigurationBuilder;
-import org.ehcache.config.builders.CacheManagerBuilder;
-import org.ehcache.config.builders.ResourcePoolsBuilder;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
